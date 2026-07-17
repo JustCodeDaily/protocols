04017ec844e60af8c15865e2c1cafefe972f8009
Baymax <justcodedaily@gmail.com>
2026-07-17 17:26:17 +0100

    :passport_control: Add Login - MS, Google, GitHub

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -1,10 +1,8 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/lib/supabase/server";
-
 export async function GET(request) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
-
   if (code) {
     const { error } = await (await createClient()).auth.exchangeCodeForSession(code);
     if (error) {
@@ -13,6 +11,5 @@ export async function GET(request) {
       return NextResponse.redirect(loginUrl);
     }
   }
-
   return NextResponse.redirect(new URL("/", url.origin));
 }
